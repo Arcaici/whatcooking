7aa331a24124bb15e78388aab57a6d1a3fa8faf9
Arcaici <43611155+Arcaici@users.noreply.github.com>
2022-11-08 15:09:47 +0100

    Updaated respect to featureExctraction_Cleaning_Encoding.py changes and changed measurament score metrics to micro instead of weighted

--- softmaxRegression.py
+++ softmaxRegression.py
@@ -1,3 +1,10 @@
+#introduction
+#I assumed that the tuning about NLP extraction obtained from naive bayes
+# it's the best also for softmax regression.
+
+#Model Tuning
+#i will tune the model over penalty type and c param. of regularization
+
 import pandas as pd
 from matplotlib import pyplot as plt
 
@@ -11,48 +18,37 @@ from sklearn.pipeline import make_pipeline
 from sklearn.linear_model import LogisticRegression
 
 
-print('\n Loading data... \n')
+print('Loading data... \n')
 
 pd.set_option("display.max_columns", None)
 
 
 data = pd.DataFrame(pd.read_json('train.json/train.json'))
 
-print('\n Cleaning data... \n')
+print('Cleaning data... \n')
 
 
-data = pd.DataFrame(ingredientsExtraction(data))
+data = pd.DataFrame(ingredientsExtraction(data, 'extraction'))
 
 X = pd.Series(data['ingredients'])
 t = pd.Series(data['cuisine'])
 target_names = t.drop_duplicates().array
 
-print('\n Encoding labels\n')
+print('Encoding labels... \n')
 t, le = encodeCusine(t)
 t = pd.Series(t)
 
-print('\n Splitting dataset (train, validation, test)... \n')
+print('Splitting dataset (train, validation, test)... \n')
 
 X_train, X_temp, t_train, t_temp = train_test_split( X, t, test_size=0.2, random_state=42)
 X_val, X_test, t_val, t_test = train_test_split(X_temp, t_temp, test_size=0.5, random_state= 42)
 
-print('\n Model selection...\n')
+print('Model selection...\n')
 
 C = np.arange(0.2,1.2,0.2)
 
 penalty =["l2", "l1"]
-
 best_score_avg = 0
-best_c = 0
-best_p = ''
-
-clf = make_pipeline(TfidfVectorizer(min_df=50, max_df=6000, ngram_range=(1, 2))
-                    , LogisticRegression( random_state=0, solver='saga', penalty='none', class_weight=None,
-                                         multi_class='multinomial'))
-clf.fit(X_train, t_train)
-labels_val = clf.predict(X_val)
-score_avg_none = f1_score(t_val, labels_val, average='weighted')
-
 
 for p in penalty:
     for c in C:
@@ -61,24 +57,19 @@ for p in penalty:
                                              multi_class='multinomial'))
         clf.fit(X_train, t_train)
         labels_val = clf.predict(X_val)
-        score_avg = f1_score(t_val, labels_val, average='weighted')
+        score_avg = f1_score(t_val, labels_val, average='micro')
         if score_avg > best_score_avg :
             best_score_avg = score_avg
             best_c = c
             best_p = p
 
-if best_score_avg < score_avg_none:
-    best_score_avg = score_avg_none
-    best_p = 'none'
-    best_c = 1
-print(f'best regularization = {best_c}')          #-> 1
-print(f'best regularization = {best_p}')          #-> l2
-print(f' with f1_weighted of = {best_score_avg}') #-> 0.765
+print(f'best regularization factor = {best_c}')         #-> 1
+print(f'best regularization penalty = {best_p}')        #-> l2
+print(f' with f1_weighted of = {best_score_avg}')       #-> 0.772
 
-# best_c = 1
-# best_p = "l2"
 
-print('\n Testing data on D_test... \n')
+
+print('Testing data on D_test... \n')
 
 clf = make_pipeline(TfidfVectorizer(min_df=50, max_df=6000, ngram_range=(1, 2))
                     , LogisticRegression(C= best_c ,random_state=0, solver='saga', penalty= best_p,
@@ -90,7 +81,7 @@ t_train = pd.concat([t_train, t_val])
 clf.fit(X_train, t_train)
 labels = clf.predict(X_test)
 
-print('\n Final Score \n')
+print('Final Score \n')
 print(classification_report(t_test, labels, target_names= target_names))  # -> 0.77
 
 
@@ -110,3 +101,13 @@ plt.xlabel('true label')
 plt.ylabel('predicted labels')
 # plt.savefig('softmax_confusionMatrix', bbox_inches='tight')
 plt.show()
+
+#conclusion
+#the model selection results still is:
+#       best regularization factor  -> 1.0
+#       best regularization penalty -> l2
+#       best_avg_score              -> 0.77
+# thats results to be the same respect to f1-weighted-score used in the first approach to model tunning.
+#
+#Observation:
+#in sktlearn->classification_report f1-micro-score is = to accuracy.
